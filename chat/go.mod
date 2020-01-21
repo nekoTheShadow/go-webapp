182fbdaaf4ba15a7db074ca93feaf38b5e128311
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-21 23:45:11 +0900

    1.2.1 クライアントのモデル化

--- chat/go.mod
+++ chat/go.mod
@@ -1,3 +1,5 @@
 module chat
 
 go 1.13
+
+require github.com/gorilla/websocket v1.4.1
--- chat/main.go
+++ chat/main.go
@@ -11,19 +11,7 @@ import "html/template"
 import "path/filepath"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう
-				</body>
-			</html>
-		`))
-	})
-
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
