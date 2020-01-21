1d74bab97001524ddc785c2a48ae339e3f0902bd
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-21 23:33:39 +0900

    1.1.1 テンプレート

--- chat/main.go
+++ chat/main.go
@@ -4,6 +4,12 @@ import "net/http"
 
 import "log"
 
+import "sync"
+
+import "html/template"
+
+import "path/filepath"
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
@@ -21,4 +27,17 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
