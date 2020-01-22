fa48c72c65810ffb3f287d4c14ddcc70c6e821a3
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-22 23:07:58 +0900

    1.4.1.2 ユニットテスト

---
+++ chat/trace/tracer_test.go
@@ -0,0 +1,18 @@
+package trace
+
+import "testing"
+
+import "bytes"
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newから戻り値がnilです。")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
+}
