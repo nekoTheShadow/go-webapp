9114c8c780c300f9a9aeefe111bd06960e9a8e04
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-25 17:25:41 +0900

    3.1.4 ログアウト

--- chat/main.go
+++ chat/main.go
@@ -34,6 +34,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	go r.run()
 	log.Println("Webサーバを開始します。ポート: " + *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
