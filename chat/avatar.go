1e23495bb8a9a3a64c93790476d6ee5da6568f6b
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-25 18:12:59 +0900

    3.2.1.1 認証サービスを使ったAvatarの実装

--- chat/avatar.go
+++ chat/avatar.go
@@ -7,3 +7,14 @@ var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できませ�
 type Avatar interface {
 	GetAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct{}
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
