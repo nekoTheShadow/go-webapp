2d160f19517ec7b007930eed0c2cefe4a44bb905
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-26 16:05:48 +0900

    3.3.5.1 他形式のファイルへの対応

--- chat/avatar.go
+++ chat/avatar.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io/ioutil"
+	"path/filepath"
 )
 
 var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません")
@@ -43,6 +45,16 @@ var UseFileSystemAvatar FileSystemAvatar
 func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
 	if userid, ok := c.userData["userid"]; ok {
 		if useridStr, ok := userid.(string); ok {
+			if files, err := ioutil.ReadDir("avatars"); err == nil {
+				for _, file := range files {
+					if file.IsDir() {
+						continue
+					}
+					if match, _ := filepath.Match(useridStr+"*", file.Name()); match {
+						return "/avatars/" + file.Name(), nil
+					}
+				}
+			}
 			return "/avatars/" + useridStr + ".jpg", nil
 		}
 	}
