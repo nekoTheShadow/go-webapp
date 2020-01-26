74891a5b48eb76bb8d2639a0296b6e8158117cbf
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-26 17:50:42 +0900

    4.2.1 sprinkle

---
+++ sprinkle/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+const otherWord = "*"
+
+var taransforms = []string{
+	otherWord,
+	otherWord,
+	otherWord,
+	otherWord,
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	"get" + otherWord,
+	"go" + otherWord,
+	"lets " + otherWord,
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		t := taransforms[rand.Intn(len(taransforms))]
+		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+	}
+}
