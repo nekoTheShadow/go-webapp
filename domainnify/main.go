2a74c05cc07c287d963302b83d62a5a8801f46e8
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-26 20:58:45 +0900

    4.2.2.1 練習問題(トップレベルドメインのカスタマイズ)

--- domainnify/main.go
+++ domainnify/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -10,11 +11,26 @@ import (
 	"unicode"
 )
 
-var tlds = []string{"com", "net"}
+type stringSlice []string
+
+func (s *stringSlice) String() string {
+	return fmt.Sprint("%v", *s)
+}
+
+func (s *stringSlice) Set(v string) error {
+	*s = append(*s, v)
+	return nil
+}
 
 const allowedChars = "abcdefghijklmnopqrstuvwxyz01234567890_-"
 
 func main() {
+	// tオプションで複数のTLDを指定できるようにする。
+	// 例: go run main.go -t com -t net
+	var tlds stringSlice
+	flag.Var(&tlds, "t", "add top level domain")
+	flag.Parse()
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
