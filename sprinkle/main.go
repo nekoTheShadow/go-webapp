ea8a71d481ff0f6a38156728237fbea2cd0dd994
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-26 18:06:32 +0900

    4.2.1.1 練習問題(変換方法のカスタマイズ)

--- sprinkle/main.go
+++ sprinkle/main.go
@@ -5,26 +5,33 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
 const otherWord = "*"
 
-var taransforms = []string{
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
-	"lets " + otherWord,
-}
-
 func main() {
+	// sprinkle.exe のフルパスを取得する。
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	// sprinkle.exe と同じディレクトリにある otherword.txt を読み込む。
+	fp, err := os.Open(filepath.Join(filepath.Dir(exe), "otherword.txt"))
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+	reader := bufio.NewScanner(fp)
+
+	taransforms := []string{}
+	for reader.Scan() {
+		taransforms = append(taransforms, reader.Text())
+	}
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
