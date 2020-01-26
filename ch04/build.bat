cd7e8472f9644131529653b5cc8f14016f6643c7
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-26 22:42:02 +0900

    4.2.4.3 ドメイン名の提案

---
+++ ch04/build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM 環境変数BHT_APIKEYに次のURLで発行したAPIKEYを設定すること: https://words.bighugelabs.com/
+REM 設定例: set BHT_APIKEY=1234567890abcd
+
+set workspace=%~dp0
+
+for %%f in (coolify domainnify sprinkle synonyms) do (
+    cd %workspace%
+    del /Q %%f.exe
+    cd ..\%%f
+    go build -o %workspace%%%f.exe
+)
+
+cd %workspace%
+synonyms.exe | sprinkle.exe | coolify.exe | domainnify.exe -t com -t net
