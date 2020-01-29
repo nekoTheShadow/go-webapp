928d06b1b5148f1d8ebb2754ae037f3e9894f5eb
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-30 00:30:33 +0900

    5.3.3 Twitterからの読み込み

--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -20,6 +22,10 @@ var authClient *oauth.Client
 var creds *oauth.Credentials
 var authSetupOnce sync.Once
 var httpClient *http.Client
+
+type tweet struct {
+	Text string
+}
 
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil {
@@ -79,4 +85,47 @@ func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
 	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
 	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
 	return httpClient.Do(req)
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("選択肢の読み込みに失敗しました:", err)
+		return
+	}
+
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("URLの解析に失敗しました:", err)
+		return
+	}
+
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("検索のリクエストの作成に失敗しました:", err)
+		return
+	}
+
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Panicln("検索のリクエストに失敗しました:", err)
+		return
+	}
+
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var tweet tweet
+		if err := decoder.Decode(&tweet); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(strings.ToLower(tweet.Text), strings.ToLower(option)) {
+				log.Println("投票:", option)
+				votes <- option
+			}
+		}
+	}
 }
