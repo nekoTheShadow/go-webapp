20bace42565e302db8a9038992a69df73193508c
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-30 22:18:56 +0900

    5.3.5 穏やかな起動と終了

--- twittervotes/go.mod
+++ twittervotes/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
+	github.com/nsqio/go-nsq v1.0.8
 	github.com/stretchr/codecs v0.0.0-20170403063245-04a5b1e1910d // indirect
 	github.com/stretchr/gomniauth v0.0.0-20170717123514-4b6c822be2eb
 	github.com/stretchr/objx v0.2.0 // indirect
--- twittervotes/main.go
+++ twittervotes/main.go
@@ -2,11 +2,53 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
+	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
 )
 
 var db *mgo.Session
+
+func main() {
+	var stoplock sync.Mutex
+	stop := false
+	stopChan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("停止します…")
+		stopChan <- struct{}{}
+		CloseCon()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	votes := make(chan string)
+	publisherStopChan := publishVotes(votes)
+	twitterStopChan := startTwitterStream(stopChan, votes)
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			CloseCon()
+			stoplock.Lock()
+			if stop {
+				stoplock.Unlock()
+				break
+			}
+			stoplock.Unlock()
+		}
+	}()
+	<-twitterStopChan
+	close(votes)
+	<-publisherStopChan
+}
 
 type poll struct {
 	Options []string
@@ -33,4 +75,19 @@ func loadOptions() ([]string, error) {
 	}
 	iter.Close()
 	return options, iter.Err()
+}
+
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote))
+		}
+		log.Println("Publisher: 停止中です")
+		pub.Stop()
+		log.Println("Publisher: 停止しました")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
 }
