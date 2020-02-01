6ec950d0e6bb6b0f887c7d817bb0e8f0fe47f065
hajime nakamura <h.nakamura0903@gmail.com>
2020-02-01 17:18:27 +0900

    5.4.1 データベースへの接続

--- counter/main.go
+++ counter/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"log"
 	"os"
 )
 
@@ -20,4 +22,16 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+
+	log.Println("データベースに接続します")
+	db, err := mgo.Dial("192.168.99.100")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Panicln("データベース接続を閉じます")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
 }
