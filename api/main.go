df9c02c27c1e7eb78e7a70739bd8696e20ff2581
hajime nakamura <h.nakamura0903@gmail.com>
2020-02-02 12:26:13 +0900

    6.3.2 データベースのセッション

--- api/main.go
+++ api/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
 
 func main() {}
 
@@ -10,10 +14,19 @@ func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
 			return
 		}
-		return fn(w, r)
+		fn(w, r)
 	}
 }
 
 func isValidAPIKey(key string) bool {
 	return key == "abc123"
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVars(r, "db", thisDb.DB("ballots"))
+		f(w, r)
+	}
 }
