fa04f8ba09881ee35718f8cebce923021cf9c4a9
hajime nakamura <h.nakamura0903@gmail.com>
2020-02-02 13:05:02 +0900

    6.6 APIを提供するシンプルなmain関数

--- api/go.mod
+++ api/go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/nsqio/go-nsq v1.0.8 // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+	gopkg.in/tylerb/graceful.v1 v1.2.15
 )
--- api/main.go
+++ api/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
+	"time"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/tylerb/graceful.v1"
 )
 
-func main() {}
+func main() {
+	var addr = flag.String("addr", ":8080", "エンドポイントのアドレス")
+	var mongo = flag.String("mongo", "localhost", "MongoDBのアドレス")
+	flag.Parse()
+	log.Println("MongoDBに接続します", mongo)
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("MongoDBへの接続に失敗しました: ", err)
+	}
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/polls/", withCORS(withVars(withData(db, withAPIKey(handlePolls)))))
+	log.Println("Webサーバを開始します: ", *addr)
+	graceful.Run(*addr, 1*time.Second, mux)
+	log.Println("停止します")
+}
 
 func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
