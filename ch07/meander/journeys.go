822d5862d7729f3d1f62638c7ddc7f1eeb8b801e
hajime nakamura <h.nakamura0903@gmail.com>
2020-02-08 12:59:04 +0900

    7.2.1 Goの構造体を公開したビュー

--- ch07/meander/journeys.go
+++ ch07/meander/journeys.go
@@ -1,8 +1,17 @@
 package meander
 
+import "strings"
+
 type j struct {
 	Name       string
 	PlaceTypes []string
+}
+
+func (j *j) Public() interface{} {
+	return map[string]interface{}{
+		"name":    j.Name,
+		"journey": strings.Join(j.PlaceTypes, "|"),
+	}
 }
 
 var Journeys = []interface{}{
