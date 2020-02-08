540f10472d8fce8797adfff648a8b46dc4e09d22
hajime nakamura <h.nakamura0903@gmail.com>
2020-02-08 22:19:08 +0900

    7.3.5 URlパラメーターを解釈するハンドラー

--- ch07/main.go
+++ ch07/main.go
@@ -5,12 +5,26 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
+	})
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+
+		places := q.Run()
+		respond(w, r, places)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
