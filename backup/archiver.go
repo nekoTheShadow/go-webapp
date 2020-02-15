44d8bccc603c07d299177833009c21b5b555e105
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-02-16 02:56:35 +0900

    8.2.2 ZIP圧縮の実装

--- backup/archiver.go
+++ backup/archiver.go
@@ -1,5 +1,52 @@
 package backup
 
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type Archiver interface {
 	Archive(src, dst string) error
+}
+
+type zipper struct{}
+
+func (z *zipper) Archive(src, dst string) error {
+	if err := os.MkdirAll(filepath.Dir(dst), 0777); err != nil {
+		return err
+	}
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	w := zip.NewWriter(out)
+	defer w.Close()
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer in.Close()
+
+		f, err := w.Create(path)
+		if err != nil {
+			return err
+		}
+
+		io.Copy(f, in)
+		return nil
+	})
 }
