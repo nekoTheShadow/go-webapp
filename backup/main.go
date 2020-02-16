37463605c190519409b7cbba395412b40414f8ca
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-02-16 18:20:54 +0900

    8.3.2.2 パスの追加

--- backup/main.go
+++ backup/main.go
@@ -59,6 +59,18 @@ func main() {
 			return false
 		})
 	case "add":
+		if len(args[1:]) == 0 {
+			fatalErr = errors.New("追加するパスを指定してください")
+			return
+		}
+		for _, p := range args[1:] {
+			path := &path{Path: p, Hash: "まだアーカイブされていません"}
+			if err := col.InsertJSON(path); err != nil {
+				fatalErr = err
+				return
+			}
+			fmt.Printf("+ %s\n", path)
+		}
 	case "remove":
 	}
 }
